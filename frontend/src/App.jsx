6ec04388496ad363700bd2c1e4a6e940a439f974
Jeifre Salcedo <jeifresalcedo318@gmail.com>
2026-07-13 20:33:25 -0500

    [FRONT] AuthContext y manejo de tokens

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
+import AppRouter from "./routes/AppRouter";
+import { useAuth } from "./context/AuthContext";
+
 function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-blue-100">
-      <h1 className="text-5xl font-bold text-blue-900">
-        Bienestar UCC
-      </h1>
-    </div>
-  );
+  const { loading } = useAuth();
+
+  // Esperar mientras se carga la sesión almacenada
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-slate-100">
+        <h1 className="text-3xl font-bold text-cyan-600">
+          Cargando...
+        </h1>
+      </div>
+    );
+  }
+
+  return <AppRouter />;
 }
 
 export default App;
